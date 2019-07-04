ec5180b47bb30d0847fcd441a452fa700d3811a8
dsol0115 <dsol0115@naver.com>
2019-07-04 22:26:57 +0900

    typecast functions

---
+++ typecast.js
@@ -0,0 +1,12 @@
+//Casting String to Number
+var str = "123456";
+var strToInt  = Number(str);
+
+console.log(strToInt+1)
+
+//Casting Number to String
+
+var num = 123
+var numToStr = String(num)+"0"
+
+console.log(numToStr);
